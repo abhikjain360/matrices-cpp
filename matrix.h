0e8333b350fbe13732772542f3940b2c187f2630
abhikjain360 <abhikjain360@gmail.com>
2020-06-15 11:24:04 +0530

    multithread mul, comments

--- matrix.h
+++ matrix.h
@@ -1,11 +1,9 @@
 #ifndef MATRIX_H
 #define MATRIX_H
 
-#include "threadpool.h"
+#include "threadpool.h" 	// for multithreading
 
-#include <cmath>
-#include <functional>
-#include <iostream>
+#include <iostream> 		// for debugging, remove later
 
 #define spt std::shared_ptr
 #define mkspt std::shared_ptr
@@ -15,13 +13,14 @@ class matrix {
   public:
 	matrix(int rows, int cols, spt<double[]> &arr) : r(rows), c(cols), ar(arr) {}
 
-	double &operator[](int index) {
+	double &operator [] (int index) {
 		return ar[index];
 	}
 
 	matrix operator + (matrix &B) {
 		spt<double[]> arr = mkspt<double[]>(new double[r * c], [](double *p) { delete[] p; });
 
+		// scope to delete threadpool
 		{
 			threadpool tp(3);
 
@@ -47,34 +46,65 @@ class matrix {
 	matrix operator - (matrix &B) {
 		spt<double[]> arr = mkspt<double[]>(new double[r * c], [](double *p) { delete[] p; });
 
-		std::thread vec[r];
+		// scope to delete threadpool
+		{
+			threadpool tp(3);
 
-		for (int i = 0; i < r; ++i)
-			vec[i] = std::thread(
-			[] (matrix &A, matrix &B, spt<double[]> &arr, int i) {
-				for (int j = 0; j < A.r; ++j)
-					arr[i*A.c + j] = A[i*A.c + j] - B[i*A.c + j];
-			},
-				std::ref(*this),
-				std::ref(B),
-				std::ref(arr),
-				i
-			);
-
-		for (int i = 0; i < r; ++i)
-			vec[i].join();
+			for (int i = 0; i < r; ++i)
+			{
+				tp.add(
+				[] (matrix &A, matrix &B, spt<double[]> &arr, int i) {
+					for (int j = 0; j < A.c; ++j)
+						arr[i*A.c + j] = A[i*A.c + j] - B[i*A.c + j];
+				},
+					std::ref(*this),
+					std::ref(B),
+					std::ref(arr),
+					i
+				);
+			}
+		}
 
 		matrix C = matrix(r, c, arr);
 		return C;
 	}
 
-	void operator = (matrix &other) {
+	matrix operator * (matrix &B) {
+		spt<double[]> arr = mkspt<double[]>(new double[r * c], [](double *p) { delete[] p; });
+
+		// scope to delete threadpool
+		{
+			threadpool tp(3);
+
+			for (int i = 0; i < r; ++i) {
+				for (int j = 0; j < B.c; ++j) {
+					arr[i*r + j] = 0;
+					tp.add([] (matrix &A, matrix &B, spt<double[]>& arr, int i, int j) {
+						for (int k = 0; k < A.c; ++k) {
+							arr[i*B.c + j] += A[i*A.c + k] * B[k*B.c + j];
+						}
+					},
+						std::ref(*this),
+						std::ref(B),
+						std::ref(arr),
+						i, j
+					);
+				}
+			}
+		}
+
+		matrix C = matrix(r, c, arr);
+		return C;
+	}
+
+
+	void operator = (matrix other) {
 		if (this == &other)
 			return;
 
-		r 	= (&other)->r;
-		c 	= (&other)->c;
-		ar 	= (&other)->ar;
+		r 	= other.r ;
+		c 	= other.c ;
+		ar 	= other.ar ;
 	}
 
 	void print() const {
@@ -96,7 +126,7 @@ class matrix {
 
 
 matrix regular_add(matrix& A, matrix& B) {
-		spt<double[]> arr = mkspt<double[]>(new double[A.r * A.c], [](double *p) { delete[] p; });
+	spt<double[]> arr = mkspt<double[]>(new double[A.r * A.c], [](double *p) { delete[] p; });
 	matrix C(A.r, A.c, arr);
 
 	for (int i = 0;  i < A.r; ++i) {
@@ -106,11 +136,32 @@ matrix regular_add(matrix& A, matrix& B) {
 	return C;
 }
 
-void regular_sub(matrix& A, matrix& B, matrix& C) {
-	for (int i = 0; i < A.r; ++i) {
-		for (int j  = 0; j < A.c; ++j)
-			C[i*A.r + j] = A[i*A.r + j] - B[i*A.r + j];
+matrix regular_sub(matrix& A, matrix& B) {
+	spt<double[]> arr = mkspt<double[]>(new double[A.r * A.c], [](double *p) { delete[] p; });
+	matrix C(A.r, A.c, arr);
+
+	for (int i = 0;  i < A.r; ++i) {
+		for (int j = 0; j < B.c; ++j)
+			C[i*A.c + j] = A[i*A.c + j] + B[i*A.c + j];
 	}
+	return C;
+
+}
+
+matrix regular_mul(matrix& A, matrix& B) {
+	spt<double[]> arr = mkspt<double[]>(new double[A.r * A.c], [](double *p) { delete[] p; });
+	matrix C(A.r, B.c, arr);
+
+	for (int i = 0; i < A.r; ++i) {
+		for (int j = 0; j < B.c; ++j) {
+				C[i*A.r + j] = 0;
+			for (int k = 0; k < A.c; ++k) {
+				C[i*B.c + j] += A[i*A.c + k] * B[k*B.c + j];
+			}
+		}
+	}
+
+	return C;
 }
 
 
