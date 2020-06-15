99ed6f8b5a97add8a740272dd460e66eb0b64ab3
abhikjain360 <abhikjain360@gmail.com>
2020-06-15 11:43:57 +0530

    minor improvements

--- matrix.h
+++ matrix.h
@@ -11,24 +11,24 @@
 
 class matrix {
   public:
-	matrix(int rows, int cols, spt<double[]> &arr) : r(rows), c(cols), ar(arr) {}
+	matrix(u_int rows, u_int cols, spt<double[]> &arr) : r(rows), c(cols), ar(arr) {}
 
-	double &operator [] (int index) {
+	double &operator [] (u_int index) {
 		return ar[index];
 	}
 
 	matrix operator + (matrix &B) {
-		spt<double[]> arr = mkspt<double[]>(new double[r * c], [](double *p) { delete[] p; });
+		auto arr = mkspt<double[]>(new double[r * c], [](double *p) { delete[] p; });
 
 		// scope to delete threadpool
 		{
 			threadpool tp(3);
 
-			for (int i = 0; i < r; ++i)
+			for (u_int i = 0; i < r; ++i)
 			{
 				tp.add(
 				[] (matrix &A, matrix &B, spt<double[]> &arr, int i) {
-					for (int j = 0; j < A.c; ++j)
+					for (u_int j = 0; j < A.c; ++j)
 						arr[i*A.c + j] = A[i*A.c + j] + B[i*A.c + j];
 				},
 					std::ref(*this),
@@ -45,17 +45,17 @@ class matrix {
 	}
 
 	matrix operator - (matrix &B) {
-		spt<double[]> arr = mkspt<double[]>(new double[r * c], [](double *p) { delete[] p; });
+		auto arr = mkspt<double[]>(new double[r * c], [](double *p) { delete[] p; });
 
 		// scope to delete threadpool
 		{
 			threadpool tp(3);
 
-			for (int i = 0; i < r; ++i)
+			for (u_int i = 0; i < r; ++i)
 			{
 				tp.add(
-				[] (matrix &A, matrix &B, spt<double[]> &arr, int i) {
-					for (int j = 0; j < A.c; ++j)
+				[] (matrix &A, matrix &B, spt<double[]> &arr, u_int i) {
+					for (u_int j = 0; j < A.c; ++j)
 						arr[i*A.c + j] = A[i*A.c + j] - B[i*A.c + j];
 				},
 					std::ref(*this),
@@ -72,17 +72,17 @@ class matrix {
 	}
 
 	matrix operator * (matrix &B) {
-		spt<double[]> arr = mkspt<double[]>(new double[r * c], [](double *p) { delete[] p; });
+		auto arr = mkspt<double[]>(new double[r * c], [](double *p) { delete[] p; });
 
 		// scope to delete threadpool
 		{
 			threadpool tp(3);
 
-			for (int i = 0; i < r; ++i) {
-				for (int j = 0; j < B.c; ++j) {
+			for (u_int i = 0; i < r; ++i) {
+				for (u_int j = 0; j < B.c; ++j) {
 					arr[i*r + j] = 0;
-					tp.add([] (matrix &A, matrix &B, spt<double[]>& arr, int i, int j) {
-						for (int k = 0; k < A.c; ++k) {
+					tp.add([] (matrix &A, matrix &B, spt<double[]>& arr, u_int i, u_int j) {
+						for (u_int k = 0; k < A.c; ++k) {
 							arr[i*B.c + j] += A[i*A.c + k] * B[k*B.c + j];
 						}
 					},
@@ -111,8 +111,8 @@ class matrix {
 	}
 
 	void print() const {
-		for (int i = 0; i < r; ++i) {
-			for (int j = 0; j < c; ++j) {
+		for (u_int i = 0; i < r; ++i) {
+			for (u_int j = 0; j < c; ++j) {
 				std::cout << ar[i * c + j] << "\t";
 			}
 			std::cout << "\n";
@@ -121,7 +121,7 @@ class matrix {
 	}
 
   public:
-	int r, c;
+	u_int r, c;
 
   private:
 	spt<double[]> ar;
@@ -129,10 +129,10 @@ class matrix {
 
 
 matrix regular_add(matrix& A, matrix& B) {
-	spt<double[]> arr = mkspt<double[]>(new double[A.r * A.c], [](double *p) { delete[] p; });
+	auto arr = mkspt<double[]>(new double[A.r * A.c], [](double *p) { delete[] p; });
 
-	for (int i = 0;  i < A.r; ++i) {
-		for (int j = 0; j < B.c; ++j)
+	for (u_int i = 0;  i < A.r; ++i) {
+		for (u_int j = 0; j < B.c; ++j)
 			arr[i*A.c + j] = A[i*A.c + j] + B[i*A.c + j];
 	}
 
@@ -142,10 +142,10 @@ matrix regular_add(matrix& A, matrix& B) {
 }
 
 matrix regular_sub(matrix& A, matrix& B) {
-	spt<double[]> arr = mkspt<double[]>(new double[A.r * A.c], [](double *p) { delete[] p; });
+	auto arr = mkspt<double[]>(new double[A.r * A.c], [](double *p) { delete[] p; });
 
-	for (int i = 0;  i < A.r; ++i) {
-		for (int j = 0; j < B.c; ++j)
+	for (u_int i = 0;  i < A.r; ++i) {
+		for (u_int j = 0; j < B.c; ++j)
 			arr[i*A.c + j] = A[i*A.c + j] + B[i*A.c + j];
 	}
 
@@ -156,12 +156,12 @@ matrix regular_sub(matrix& A, matrix& B) {
 }
 
 matrix regular_mul(matrix& A, matrix& B) {
-	spt<double[]> arr = mkspt<double[]>(new double[A.r * A.c], [](double *p) { delete[] p; });
+	auto arr = mkspt<double[]>(new double[A.r * A.c], [](double *p) { delete[] p; });
 
-	for (int i = 0; i < A.r; ++i) {
-		for (int j = 0; j < B.c; ++j) {
+	for (u_int i = 0; i < A.r; ++i) {
+		for (u_int j = 0; j < B.c; ++j) {
 				arr[i*A.r + j] = 0;
-			for (int k = 0; k < A.c; ++k) {
+			for (u_int k = 0; k < A.c; ++k) {
 				arr[i*B.c + j] += A[i*A.c + k] * B[k*B.c + j];
 			}
 		}
