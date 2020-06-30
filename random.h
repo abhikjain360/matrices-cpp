a7a50069c89a7e315b6538ac790e3beb208e54f8
abhikjain360 <abhikjain360@gmail.com>
2020-06-30 12:08:42 +0530

    random rewrite

--- random.h
+++ random.h
@@ -1,28 +1,18 @@
-#ifndef RANDOM_H
-#define RANDOM_H
+#pragma once
 
 #include "matrix.h"
+#include "quad.h"
 
-#include <stdlib.h>
-#include <ctime>
-#include <iostream>
+quad getRandomQuad
+(
+	unsigned int rows,
+	unsigned int cols,
+	unsigned int limit
+);
 
-#define spt std::shared_ptr
-#define mkspt std::shared_ptr
-
-
-matrix get_random(int rows, int cols, int limit) {
-	spt<double[]> arr = mkspt<double[]>(new double[rows * cols], [](double *p) { delete[] p; });
-	std::srand(time(0));
-
-	for (int i = 0; i < rows; ++i) {
-		for (int j = 0; j < cols; ++j) {
-			arr[i*cols + j] = (rand()%limit);
-		}
-	}
-
-	matrix C(rows, cols, arr);
-	return C;
-}
-
-#endif
+matrix getRandomMatrix
+(
+	unsigned int rows,
+	unsigned int cols,
+	unsigned int limit
+);
