01934c5871f5fb3affed123aa9df89560c6efd4d
abhikjain360 <abhikjain360@gmail.com>
2020-07-21 14:31:05 +0530

    formatting

--- normal/random.h
+++ normal/random.h
@@ -17,31 +17,31 @@ quad<T> getRandomQuad(unsigned int rows, unsigned int cols, unsigned int limit);
 template <typename T>
 matrix<T> getRandomMatrix(unsigned int rows, unsigned int cols, unsigned int limit)
 {
-	matrix<T> M(rows, cols);
+    matrix<T> M(rows, cols);
 
-	std::srand(time(0));
+    std::srand(time(0));
 
-	for (unsigned int i = 0; i < rows; ++i)
-		for (unsigned int j = 0; j < cols; ++j) {
-			M[i*cols + j] = rand() % limit + 1;
-			//M[i*cols + j] *= rand()%2 ? 1 : -1;
-		}
+    for (unsigned int i = 0; i < rows; ++i)
+        for (unsigned int j = 0; j < cols; ++j) {
+            M[i*cols + j] = rand() % limit + 1;
+            //M[i*cols + j] *= rand()%2 ? 1 : -1;
+        }
 
-	return M;
+    return M;
 }
 
 template <typename T>
 quad<T> getRandomQuad(unsigned int rows,unsigned int cols,unsigned int limit)
 {
-	quad<T> M(rows, cols);
+    quad<T> M(rows, cols);
 
-	std::srand(time(0));
+    std::srand(time(0));
 
-	for (unsigned int i = 0; i < rows; ++i)
-		for (unsigned int j = 0; j < cols; ++j) {
-			M[i*cols + j] = rand() % limit + 1;
-			//M[i*cols + j] *= rand()%2 ? 1 : -1;
-		}
+    for (unsigned int i = 0; i < rows; ++i)
+        for (unsigned int j = 0; j < cols; ++j) {
+            M[i*cols + j] = rand() % limit + 1;
+            //M[i*cols + j] *= rand()%2 ? 1 : -1;
+        }
 
-	return M;
+    return M;
 }
